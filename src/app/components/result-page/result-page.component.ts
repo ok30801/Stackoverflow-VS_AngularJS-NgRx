918cd7cc9845e57e53f2296c28420b24a6b3b63c
Oleg <53225808+ok30801@users.noreply.github.com>
2022-05-05 15:22:05 +0500

    Сортировка данных по Имени автора

--- src/app/components/result-page/result-page.component.ts
+++ src/app/components/result-page/result-page.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Pipe} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { OpenModalsService } from '../../shared/services/open-modals.service';
 import { AuthorDataSelector, StackOverflowDataSelector } from '../../store/selectors/selectors';
+
+// @Pipe({name: 'sortArrayOfCustomObjects'})
 
 @Component({
   selector: 'app-result-page',
   templateUrl: './result-page.component.html',
   styleUrls: ['./result-page.component.scss']
 })
-export class ResultPageComponent implements OnInit {
+export class ResultPageComponent implements OnInit  {
 
   focused!: true
   form!: FormGroup
   formError = false
+  isSorting = false
+  sortingData: any
 
   public searchData$ = this.store.select(StackOverflowDataSelector)
   public authorData$ = this.store.select(AuthorDataSelector)
@@ -37,5 +41,21 @@ export class ResultPageComponent implements OnInit {
 
   handleClickTag(id: string) {
     this.openModal.handleClickTag(id)
+  }
+
+  sorting() {
+    this.isSorting = !this.isSorting
+    this.searchData$.subscribe(item => {
+      const array = [...item]
+      this.sortingData = array.sort((a: any, b: any) => {
+        let nameA = a.owner.display_name.toLowerCase()
+        let nameB = b.owner.display_name.toLowerCase()
+        if (nameA < nameB)
+          return -1
+        if (nameA > nameB)
+          return 1
+        return 0
+      })
+    })
   }
 }
